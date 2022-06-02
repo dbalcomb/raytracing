1e2ff3481ef3e3b9613a7db949af45624df53353
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-03 00:42:33 +0100

    11.2. Positioning and Orienting the Camera

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ use self::camera::Camera;
 use self::color::write_color;
 use self::hittable::{HitRecord, Hittable};
 use self::hittable_list::HittableList;
-use self::material::Lambertian;
+use self::material::{Dielectric, Lambertian, Metal};
 use self::ray::Ray;
 use self::rtweekend::random_f64;
 use self::sphere::Sphere;
-use self::vec3::{Color, Point3};
+use self::vec3::{Color, Point3, Vec3};
 
 fn ray_color(ray: &Ray, world: &dyn Hittable, depth: i32) -> Color {
     // If we've exceeded the ray bounce limit, no more light is gathered.
@@ -59,26 +59,48 @@ fn main() {
 
     // World
 
-    let r = (std::f64::consts::PI / 4.0).cos();
     let mut world = HittableList::default();
 
-    let material_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
-    let material_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
+    let material_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let material_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let material_left = Rc::new(Dielectric::new(1.5));
+    let material_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 0.0));
 
     world.add(Rc::new(Sphere::new(
-        Point3::new(-r, 0.0, -1.0),
-        r,
+        Point3::new(0.0, -100.5, -1.0),
+        100.0,
+        material_ground,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(0.0, 0.0, -1.0),
+        0.5,
+        material_center,
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        0.5,
+        material_left.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.45,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
-        Point3::new(r, 0.0, -1.0),
-        r,
+        Point3::new(1.0, 0.0, -1.0),
+        0.5,
         material_right,
     )));
 
     // Camera
 
-    let cam = Camera::new(90.0, aspect_ratio);
+    let cam = Camera::new(
+        Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        20.0,
+        aspect_ratio,
+    );
 
     // Render
 
