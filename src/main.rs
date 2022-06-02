b454e697733af0c80a32c701c29fd8bf5bcf89e4
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-03 00:19:14 +0100

    10.5. Modeling a Hollow Glass Sphere

--- src/main.rs
+++ src/main.rs
@@ -79,6 +79,11 @@ fn main() {
     world.add(Rc::new(Sphere::new(
         Point3::new(-1.0, 0.0, -1.0),
         0.5,
+        material_left.clone(),
+    )));
+    world.add(Rc::new(Sphere::new(
+        Point3::new(-1.0, 0.0, -1.0),
+        -0.4,
         material_left,
     )));
     world.add(Rc::new(Sphere::new(
