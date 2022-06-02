83bacea145ab607387c782575775971e10f245e6
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-02 14:13:08 +0100

    2.1. The PPM Image Format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+
+    println!("P3\n{image_width} {image_height}\n255");
+
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.25;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            println!("{ir} {ig} {ib}");
+        }
+    }
 }
