6726af26c73e211eacc2dd329af7b4bd88e4d6d1
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-02 14:30:33 +0100

    2.3. Adding a Progress Indicator

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ fn main() {
     println!("P3\n{image_width} {image_height}\n255");
 
     for j in (0..image_height).rev() {
+        eprintln!("Scanlines remaining: {j}");
+
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -21,4 +23,6 @@ fn main() {
             println!("{ir} {ig} {ib}");
         }
     }
+
+    eprintln!("Done.");
 }
