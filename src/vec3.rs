ebf41b3b50666b68a45982651813393586756ad8
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-02 19:50:35 +0100

    8.5. True Lambertian Reflection

--- src/vec3.rs
+++ src/vec3.rs
@@ -43,6 +43,10 @@ impl Vec3 {
         }
     }
 
+    pub fn random_unit_vector() -> Self {
+        Self::random_in_unit_sphere().unit_vector()
+    }
+
     pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
