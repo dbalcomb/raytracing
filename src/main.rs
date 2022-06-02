20a5e70fdf46a2e7ca47d9f98fa939e33ab0e2e2
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-02 15:46:53 +0100

    4.2. Sending Rays Into the Scene

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,34 @@ pub mod ray;
 pub mod vec3;
 
 use self::color::write_color;
-use self::vec3::Color;
+use self::ray::Ray;
+use self::vec3::{Color, Point3, Vec3};
+
+fn ray_color(ray: &Ray) -> Color {
+    let unit_direction = ray.direction.unit_vector();
+    let t = 0.5 * (unit_direction.y + 1.0);
+
+    (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
     // Image
 
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+
+    // Camera
+
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.0;
+
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner =
+        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     // Render
 
@@ -19,11 +40,13 @@ fn main() {
         eprintln!("Scanlines remaining: {j}");
 
         for i in 0..image_width {
-            let pixel_color = Color::new(
-                i as f64 / (image_width - 1) as f64,
-                j as f64 / (image_height - 1) as f64,
-                0.25,
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::new(
+                origin,
+                lower_left_corner + u * horizontal + v * vertical - origin,
             );
+            let pixel_color = ray_color(&r);
 
             write_color(pixel_color);
         }
