094997068796ed3af5c50a1264951867a9504c78
Daniel Balcomb <daniel.balcomb@gmail.com>
2022-06-02 17:20:52 +0100

    6.5. A List of Hittable Objects

--- src/hittable.rs
+++ src/hittable.rs
@@ -19,6 +19,17 @@ impl HitRecord {
     }
 }
 
+impl Default for HitRecord {
+    fn default() -> Self {
+        Self {
+            p: Point3::default(),
+            normal: Vec3::default(),
+            t: 0.0,
+            front_face: false,
+        }
+    }
+}
+
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool;
 }
---
+++ src/hittable_list.rs
@@ -0,0 +1,44 @@
+use std::rc::Rc;
+
+use crate::hittable::{HitRecord, Hittable};
+use crate::ray::Ray;
+
+pub struct HittableList {
+    objects: Vec<Rc<dyn Hittable>>,
+}
+
+impl HittableList {
+    pub fn add(&mut self, object: Rc<dyn Hittable>) {
+        self.objects.push(object);
+    }
+
+    pub fn clear(&mut self) {
+        self.objects.clear()
+    }
+}
+
+impl Default for HittableList {
+    fn default() -> Self {
+        Self {
+            objects: Vec::new(),
+        }
+    }
+}
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, t_min: f64, t_max: f64, rec: &mut HitRecord) -> bool {
+        let mut temp_rec = HitRecord::default();
+        let mut hit_anything = false;
+        let mut closest_so_far = t_max;
+
+        for object in &self.objects {
+            if object.hit(ray, t_min, closest_so_far, &mut temp_rec) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                *rec = temp_rec;
+            }
+        }
+
+        hit_anything
+    }
+}
